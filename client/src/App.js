948057f9e9af92f8118355a4a26ed12672249961
Nick <njhuemmer@gmail.com>
2022-08-16 17:49:55 -0400

    add fetching and displaying data from spotify

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,47 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
-    // window.location.search is basically everything after localhost3000 in the url
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
+    setToken(accessToken);
 
-    console.log(accessToken)
-    console.log(refreshToken)
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-    }
-  }, [])
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+
+        console.log(data)
+
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Log in to Spotify
-        </a>
+        {!token ? (
+          <a
+            className="App-link"
+            href="http://localhost:8888/login"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Logged In!</h1>
+            <button onClick={logout}>Log Out</button>
+          </>
+        )}
       </header>
     </div>
   );
