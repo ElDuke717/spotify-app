5979d8603d01a30d51d383eca95d0ad588b841b8
Nick <njhuemmer@gmail.com>
2022-08-17 15:16:15 -0400

    add profile info to App.js, solve issue with axios error in Firefox

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
 import './App.css';
 
 function App() {
@@ -10,18 +11,14 @@ function App() {
     setToken(accessToken);
 
     const fetchData = async () => {
-      try {
-        const { data } = await getCurrentUserProfile();
-        setProfile(data);
 
-        console.log(data)
-
-      } catch (e) {
-        console.error(e);
-      }
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+      console.log(data)
     };
 
-    fetchData();
+    catchErrors(fetchData());
+
   }, []);
 
   return (
@@ -31,8 +28,6 @@ function App() {
           <a
             className="App-link"
             href="http://localhost:8888/login"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Log in to Spotify
           </a>
@@ -40,6 +35,17 @@ function App() {
           <>
             <h1>Logged In!</h1>
             <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar" />
+                )}
+              </div>
+            )}
+
           </>
         )}
       </header>
